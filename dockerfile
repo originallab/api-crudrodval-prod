98ca6ef8432009148d6b431281bc1cca2edc4a5d
leticiaOntiveros <leticiaontiveros41@gmail.com>
2025-02-24 11:42:24 -0600

    ya jala en conexion con .env

--- dockerfile
+++ dockerfile
@@ -1,17 +1,9 @@
-# Usa una imagen base de Python
+
 FROM python:3.9-slim
-
-# Establece el directorio de trabajo dentro del contenedor
 WORKDIR /backend
-
-# Copia los archivos de tu aplicación al contenedor
 COPY . .
-
-# Instala las dependencias
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Expone el puerto en el que corre tu aplicación
 EXPOSE 8000
 
-# Comando para correr tu aplicación
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- models/models.py
+++ models/models.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
 import os
 
-DB_USER = os.getenv("DB_USER", "logisticaRODVAL")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "logisticaRODVAL")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "rodval")
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener las variables de entorno
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 # Construcción del string de conexión usando f-string
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
