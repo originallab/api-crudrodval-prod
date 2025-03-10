4405fbb759fc9cc903edec30fcf9303abf8477dc
hectorlares <alejandrolacha7@gmail.com>
2025-03-10 11:18:52 -0600

    nombre

--- models/models.py
+++ models/models.py
@@ -9,7 +9,7 @@ import os
 DB_USER = os.getenv("DB_USER","mariadb")
 DB_PASSWORD = os.getenv("DB_PASSWORD","neurone123")
 DB_HOST = os.getenv("DB_HOST","localhost")
-DB_NAME = os.getenv("DB_NAME","inventarioneurone")
+DB_NAME = os.getenv("DB_NAME","neuronecosmetica")
 
 # Construcción del string de conexión usando f-string
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
